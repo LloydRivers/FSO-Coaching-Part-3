36276c85fcad56182932b05f32137ab2c6dddfbf
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2024-05-10 08:44:27 +0100

    3.2: Phonebook backend step 2

---
+++ src/formatDate.ts
@@ -0,0 +1,22 @@
+/*
+If you are wondering how I formatted the options object, this mdn link will explain, including what the keys (like weekday: "short" mean):
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat/DateTimeFormat#options
+*/
+
+const formatDateOptions = () => {
+  const now = new Date();
+  const options: Intl.DateTimeFormatOptions = {
+    weekday: "short",
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    timeZoneName: "short",
+    timeZone: "GMT",
+  };
+  return now.toLocaleString("en-GB", options);
+};
+
+export default formatDateOptions;
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
 import express, { Request, Response, Application } from "express";
 import { Server } from "http";
 import data from "./data";
+import formatDateOptions from "./formatDate";
 
 const app: Application = express();
 
 app.get("/api/persons", (req: Request, res: Response) => {
   console.log("GET /");
   res.send(data);
+});
+
+app.get("/info", (req: Request, res: Response) => {
+  const formattedDate = formatDateOptions();
+
+  const template = `
+      <p>Phonebook has info for ${data.length} people</p>
+      <p>${formattedDate}</p>
+    `;
+
+  res.send(template);
 });
 
 const startServer = (port: number, app: Application): Server => {
