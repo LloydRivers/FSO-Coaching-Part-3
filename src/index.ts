2ab8e391ee7e9acf991ade1d2fb145d8c65de6b9
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2024-05-10 09:13:14 +0100

    3.4: Phonebook backend step 4

--- src/index.ts
+++ src/index.ts
@@ -21,13 +21,30 @@ app.get("/api/persons/:id", (req: Request, res: Response) => {
   }
 });
 
+app.delete("/api/persons/:id", (req: Request, res: Response) => {
+  const id = Number(req.params.id);
+  const person = data.find((person) => person.id === id);
+
+  if (person) {
+    data.splice(data.indexOf(person), 1);
+    /*
+    From Google: A 200 OK status code indicates that the resource has been removed and it will include a message for the client that describes the status. A 204 No Content status code indicates that the resource has been removed but there is no message body to further describe the action or the status.
+    */
+    res.status(200).send(`Person with id ${id} deleted`); // This will send back a message to the client
+    // res.status(204).send(`Person with id ${id} deleted`);  This will send back an empty response
+    // res.status(204).end();  This will send back an empty response
+  } else {
+    res.status(404).send("Not found");
+  }
+});
+
 app.get("/info", (req: Request, res: Response) => {
   const formattedDate = formatDateOptions();
 
   const template = `
-      <p>Phonebook has info for ${data.length} people</p>
-      <p>${formattedDate}</p>
-    `;
+        <p>Phonebook has info for ${data.length} people</p>
+        <p>${formattedDate}</p>
+      `;
 
   res.send(template);
 });
